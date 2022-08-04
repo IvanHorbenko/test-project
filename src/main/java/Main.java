1770474d6cd422859d4dc2e4a7e09a61cc3ff25e
Ivan_Horbenko <ivan.gorbenko89@gmail.com>
2022-08-04 11:33:57 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 11;
+
+        if (num % 2 == 0) {
+            System.out.println(num + " Четное число");
+        }
+        else {
+            System.out.println(num + " Нечетное");
+        }
+
+    }
+}
